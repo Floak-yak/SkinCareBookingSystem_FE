e0cf95b8ef4136badd47b9b36b1ecfa5839af18d
ToKhuyet <banghuu7@gmail.com>
2025-02-27 13:01:14 +0700

    Add new blog and products data, update routing for About Us page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AppRoutes from "./routes";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 import "./styles/index.css";
-import AboutUs from './pages/AboutUs';
 
 const App = () => {
   return (
@@ -15,10 +14,7 @@ const App = () => {
         <Router>
           <Header />
           <main className="main-content">
-            <Routes>
-              <Route path="/" element={<AppRoutes />} />
-              <Route path="/about" element={<AboutUs />} />
-            </Routes>
+            <AppRoutes />
           </main>
           <Footer />
         </Router>
